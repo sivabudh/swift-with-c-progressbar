456e8dfeebf72d558bea64e0f8f530226c816d12
Sivabudh Umpudh <sivabudh.u@codium.co>
2018-10-27 14:48:30 +0700

    Initial commit

---
+++ Progressbar/Progressbar.h
@@ -0,0 +1,19 @@
+//
+//  Progressbar.h
+//  Progressbar
+//
+//  Created by Sivabudh Umpudh on 27/10/18.
+//  Copyright © 2018 Sivabudh Umpudh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Progressbar.
+FOUNDATION_EXPORT double ProgressbarVersionNumber;
+
+//! Project version string for Progressbar.
+FOUNDATION_EXPORT const unsigned char ProgressbarVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Progressbar/PublicHeader.h>
+
+
